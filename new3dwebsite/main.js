d9f23d4a0f9e02e055319b01f052fec52bd722db
ankuree <ankurraj1209@gmail.com>
2024-01-30 00:40:06 +0530

    wewewewe

---
+++ new3dwebsite/main.js
@@ -0,0 +1,29 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000) 
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#bg'),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+renderer.render(scene, camera);
+
+
+const geometry = new THREE.TetrahedronGeometry(10, 3);
+const material = new THREE.MeshStandardMaterial({ color: 0x00ff00, wireframe: true });
+const TetrahedronGeometry = new THREE.Mesh(geometry, material);
+scene.add(TetrahedronGeometry);
+
+function animate() {
+  requestAnimationFrame(animate);
+  TetrahedronGeometry.rotation.x += 0.01;
+  TetrahedronGeometry.rotation.y += 0.01;
+  TetrahedronGeometry.rotation.z += 0.01;
+  renderer.render(scene, camera);
+}
+
